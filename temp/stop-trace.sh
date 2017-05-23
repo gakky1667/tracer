06fe067376728ad1909913ec764dc2a801a632b3
Yuhei Suzuki <gakky@ubi.cs.ritsumei.ac.jp>
2017-05-23 17:13:55 +0900

    Delete unused files

--- temp/stop-trace.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo 0 > /sys/kernel/debug/tracing/tracing_on
-echo 0 > /sys/kernel/debug/tracing/events/enable
-cat /sys/kernel/debug/tracing/trace > trace.log
-cat /sys/kernel/debug/tracing/trace_pipe > trace_pipe.log
-
-umount /sys/kernel/debug/
